4c8c3356334811dd74b6e92909739515c79d77de
Kankai Zhang <zkk_zero@hotmail.com>
2016-09-01 14:00:41 +1000

    error exercise

---
+++ error-exercise/error-exercise.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// ErrNegativeSqrt ...
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v",
+		float64(e))
+}
+
+// Sqrt ...
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	var z = float64(5)
+	var p = float64(0)
+	for math.Abs(p-z) > 0.00001 {
+		p = z
+		z = z - ((z*z - x) / (2 * z))
+		//fmt.Println(z)
+	}
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
