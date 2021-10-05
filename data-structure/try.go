aeac43c972775337758950b8be294215114db9f8
Jeremy Zhang <zkk_zero@hotmail.com>
2021-10-06 07:03:46 +1100

    Add more ops

--- data-structure/try.go
+++ data-structure/try.go
@@ -6,13 +6,22 @@ func main() {
 	var a int = 5
 	var b float32 = 23.235
 	const pi float64 = 3.14159265354
-
-	x, y := 13, 14
-
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(pi)
 
-	fmt.Println(x, y)
+	x, y := 13, 14
 	fmt.Println("x+y =", x+y)
+	fmt.Println("x-y =", x-y)
+	fmt.Println("x*y =", x*y)
+	fmt.Println("x/y =", x/y)
+	fmt.Println("x mod y =", x%y)
+
+	// floor div and math div
+	fmt.Println(3/4, "vs", float32(3/4), "vs", float32(3)/float32(4))
+	// multi 2 same as Python
+	fmt.Println(7, "*2 ops", 7<<1)
+
+	TRUE, FALSE := true, false
+	fmt.Println(TRUE && FALSE, TRUE || FALSE, !TRUE, !FALSE)
 }
