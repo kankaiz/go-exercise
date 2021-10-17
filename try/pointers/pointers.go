6ebf109cfb2ecc847eeed5b02c3e879b656bcb03
Jeremy Zhang <zkk_zero@hotmail.com>
2021-10-17 23:00:13 +1100

    Add more pointers exer

--- try/pointers/pointers.go
+++ try/pointers/pointers.go
@@ -11,6 +11,17 @@ func main() {
 
 	change(&x)
 	fmt.Println(x)
+
+	var b *int
+	// no memory allocated
+	fmt.Println(b)
+	// panic: runtime error: invalid memory address or nil pointer dereference
+	// fmt.Println(*b)
+	c := new(int)
+	// memory allocated
+	fmt.Println(c)
+	// no panic
+	fmt.Println(*c)
 }
 
 func noChange(x int) {
