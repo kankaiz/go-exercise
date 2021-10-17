56859df9e4b73e8efd745ee18e0ed4f5b788c08d
Jeremy Zhang <zkk_zero@hotmail.com>
2021-10-17 16:32:00 +1100

    Add range offset as bytes

--- try/control.go
+++ try/control.go
@@ -5,6 +5,12 @@ import "fmt"
 func main() {
 	defer firstRun()
 	defer secondRun()
+
+	s := "💪 🔥"
+	// the offset is measured in bytes: 0,4,5
+	for k, v := range s {
+		fmt.Println(k, v, string(v))
+	}
 }
 
 func firstRun()  { fmt.Println("first") }
