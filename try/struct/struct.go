4937eef9f46e025a3230073e4bf2b0ab05688c61
Jeremy Zhang <zkk_zero@hotmail.com>
2021-10-18 23:06:51 +1100

    Try more structs

--- try/struct/struct.go
+++ try/struct/struct.go
@@ -5,13 +5,33 @@ import (
 	"math"
 )
 
+// Capped Foo is public struct
+type Foo struct {
+	A int
+	b string
+}
+
 func main() {
+	// Strusts are not Objects!
+	f := Foo{
+		A: 20,
+	}
+	//f2 full copy of f
+	var f2 Foo
+	f2 = f
+	f2.A = 100
+	fmt.Println(f2.A, f.A)
+
+	//f3 points to the same memory of f
+	var f3 *Foo = &f
+	f3.A = 200
+	fmt.Println(f3.A, f.A)
+
 	rec1 := Rectangle{10, 5}
 	cir := Circle{7}
 
 	// fmt.Println(rec1.height)
-	fmt.Println(rec1.rarea())
-	fmt.Println(rec1.area())
+	fmt.Println(rec1.rarea(), rec1.area())
 	// fmt.Println(getArea(rec1))
 	// cannot use rec1 (variable of type Rectangle) as Shape value in argument to getArea:
 	// missing method area (area has pointer receiver)
