77b14b234b16e49689363c7f0965e2bd4c0d1c45
Jeremy Zhang <zkk_zero@hotmail.com>
2023-01-04 21:36:36 +1100

    Add IntTree snippets

---
+++ inttree/inttree.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type IntTree struct {
+	val         int
+	left, right *IntTree
+}
+
+func (it *IntTree) Insert(val int) *IntTree {
+	if it == nil {
+		return &IntTree{val: val}
+	}
+	if val < it.val {
+		it.left = it.left.Insert(val)
+	} else if val > it.val {
+		it.right = it.right.Insert(val)
+	}
+	return it
+}
+
+func main() {
+	var it IntTree
+	fmt.Println(it)
+	it.Insert(3)
+	fmt.Println(it)
+}
